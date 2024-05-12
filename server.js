009e874e0cbcad2c5ee25bfdedf395f01c3ebe3c
Punit Munnangi <punitram@gmail.com>
2024-05-11 20:06:33 -0700

    changed server.js to get rid of an equal sign

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let http = require('http');
 
 /* Assume that we are running on Heroku */
 let port = process.env.PORT;
-let directory = __dirname = + '/public';
+let directory = __dirname + '/public';
 
 /* If we aren't on Heroku, then we need to adjust our port and directory */
 if ((typeof port == 'undefined') || (port === null)){
